5919ab3d358e5a0043ce2a702a81b386299c7b68
yahgoam <yahgostm@hotmail.com>
2019-07-09 16:51:06 -0300

    finaliza scripts com horario

--- dataFramesandPlots.py
+++ dataFramesandPlots.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-colnames = ['index', 'humidity', 'temp1','pressure','temp2', 'light_lvl']
+
+colnames = ['time', 'index', 'humidity', 'temp1','pressure','temp2', 'light_lvl']
 data = pd.read_csv("logFile.csv",names=colnames, header=None)
 data = data.set_index(['index'],drop=True)
 
@@ -12,5 +14,10 @@ new_index2 = [i for i in range(len(sensor_2.index))]
 sensor_1['new_index'] = new_index
 sensor_2['new_index'] = new_index2
 
-sensor_1 = sensor_1.set_index(['new_index'], drop = True)
-sensor_2 = sensor_2.set_index(['new_index'], drop = True)
+sensor_1['time'] = pd.to_datetime(sensor_1['time'], format='%H:%M:%S')
+sensor_1.set_index('time', inplace=True)
+sensor_2['time'] = pd.to_datetime(sensor_2['time'], format='%H:%M:%S')
+sensor_2.set_index('time', inplace=True)
+
+sensor_1['humidity'].plot()
+plt.show()
--- plotAndLog.py
+++ plotAndLog.py
@@ -83,10 +83,13 @@ while True:
     lines = ser.readline().decode('ascii')
         #print(str(lines))
     if (len(lines.rstrip()) ==  34):
+        presentTime = time.gmtime()
+        hourMin = "{}:{}:{},".format(presentTime.tm_hour, presentTime.tm_min,presentTime.tm_sec)
         logFile = open("logFile.csv","a")
-        logFile.write(str(lines))
+        logFile.write(str(hourMin + lines))
         logFile.close()
         #end log
         #time.sleep(1)
 
 #Humidity = 44.56%, temp_h = 28.37F, Pressure = 100603.50Pa, light_lvl = 1.48V, VinPin = 4.20V
+#time.struct_time(tm_year=1970, tm_mon=1, tm_mday=1, tm_hour=0, tm_min=0, tm_sec=0, tm_wday=3, tm_yday=1, tm_isdst=0)
